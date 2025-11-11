2e81df44075b6b1d02fdb59dc9b345a6b94b3b8f
Aarsh <aarshm21@gmail.com>
2025-11-11 23:47:59 +0530

    Run Final Experiment

--- main.py
+++ main.py
@@ -185,9 +185,12 @@ async def run_mas_test(image_path: str):
 async def main():
     
     IMAGE_FILES_TO_TEST = [
-        "blurred_dataset\\test_image_blur_10.jpg",
-        "blurred_dataset\\test_image_blur_15.jpg",
-        "blurred_dataset\\test_image_blur_20.jpg",
+        "blurred_dataset\\test_image_blur_0.jpg",
+        "blurred_dataset\\test_image_blur_2.jpg",
+        "blurred_dataset\\test_image_blur_5.jpg"
+        # "blurred_dataset\\test_image_blur_10.jpg",
+        # "blurred_dataset\\test_image_blur_15.jpg",
+        # "blurred_dataset\\test_image_blur_20.jpg",
     ]
     NUM_RUNS = 3 # Set to 3-5 for real experiment
     
